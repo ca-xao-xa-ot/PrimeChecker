e0ffcf0182c1720ea422775e306b7d64d7a591c8
nhom3_giangphuong <thitvitham1314@gmail.com>
2026-08-28 08:02:40 +0700

    Initial commit

---
+++ src/test/java/PrimeCheckerTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class PrimeCheckerTest {
+
+    @Test
+    void testNumberLessThanTwo() {
+        assertFalse(PrimeChecker.isPrime(1));
+    }
+
+    @Test
+    void testCompositeNumber() {
+        assertFalse(PrimeChecker.isPrime(4));
+    }
+
+    @Test
+    void testPrimeNumber() {
+        assertTrue(PrimeChecker.isPrime(5));
+    }
+}
